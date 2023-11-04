1e9b461a905fa8f6cb2b1e0acebcbb98813a1616
Chade Powell <lashaepowell98@gmail.com>
2023-11-04 12:23:14 -0500

    Add prompt with yes or no question on button click

---
+++ app.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('button');
+
+function showPrompt(event) {
+        alert('You clicked OK. Proceeding...');
+
+        const userResponse = prompt('Do you want to proceed? (Yes or No)').toLowerCase();
+        if (userResponse === 'yes') {
+            alert('You clicked Yes!');
+        } else if (userResponse === 'no') {
+            alert('You clicked No!');
+        } else {
+            alert('Invalid response. Please enter Yes or No.');
+        }
+    }
+    buttons.forEach(button => {
+        button.addEventListener('click', showPrompt);
+    });
