c1a1fba5da5d8b1416fea123d6af1c4f7e73581d
Chade Powell <lashaepowell98@gmail.com>
2023-11-07 13:48:23 -0600

    add menu toggle

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const buttons = document.querySelectorAll('button');
 
 function showPrompt(event) {
-        alert('You clicked OK. Proceeding...');
+    alert('You clicked OK. Proceeding...');
 
-        const userResponse = prompt('Do you want to proceed? (Yes or No)').toLowerCase();
-        if (userResponse === 'yes') {
-            alert('You clicked Yes!');
-        } else if (userResponse === 'no') {
-            alert('You clicked No!');
-        } else {
-            alert('Invalid response. Please enter Yes or No.');
-        }
+    const userResponse = prompt('Do you want to proceed? (Yes or No)').toLowerCase();
+    if (userResponse === 'yes') {
+        alert('You clicked Yes!');
+    } else if (userResponse === 'no') {
+        alert('You clicked No!');
+    } else {
+        alert('Invalid response. Please enter Yes or No.');
     }
-    buttons.forEach(button => {
-        button.addEventListener('click', showPrompt);
-    });
+}
+buttons.forEach(button => {
+    button.addEventListener('click', showPrompt);
+});
